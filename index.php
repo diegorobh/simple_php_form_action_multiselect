c21e9687fa063c24a290e3a3d0215c08f4d99eeb
diegoparedes <diegorobh@gmail.com>
2019-12-09 21:24:12 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+
+<?php
+
+echo "
+<form action=\"./test.php\" method=\"post\">
+<select id=\"resp_fiscales\" name=\"resp_fiscales[]\" multiple size=\"4\" style=\"width: 300px\">
+<option value=\"\">-- NINGUNO --</option>
+<option value=\"O-06\">O-06</option>
+<option value=\"O-07\">O-07</option>
+                     </select>
+<input type=\"submit\"/>
+</form>
+";
+
+ ?>
+
+ </body>
+ </html>
---
+++ test.php
@@ -0,0 +1,5 @@
+<?php
+
+print_r($_POST["resp_fiscales"]);
+
+ ?>
